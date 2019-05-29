acc641a2534669206b13b501aa6b0b2814ce75bb
Kaustuva Ghosh <ghosh.kaustuva@gmail.com>
2019-05-29 18:27:42 +0000

    initial commit

---
+++ app.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const app =express();
+const bodyParser= require("body-parser");
+const cookieParser= require("cookie-parser");
+
+
+app.set("view engine", "ejs");
+app.use(express.static(__dirname+"/public"));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+var usercount=0;
+app.use(function(req, res, next){
+    // check if client sent cookie
+      var cookie = req.cookies.usercount;
+      if (cookie === undefined)
+      {
+        // no: set a new cookie
+        res.cookie('usercount',usercount, { maxAge: 900000, httpOnly: true });
+        console.log('cookie created successfully');
+        usercount++;
+      } 
+      else
+      {
+        // yes, cookie was already present 
+        console.log('cookie exists', cookie);
+      } 
+    next();
+});
+
+app.get("/",function(req,res){
+    res.render("index",{usercount:usercount});
+});
+
+
+app.listen(process.env.PORT, process.env.IP, function(req,res){
+  console.log('listening on'+process.env.PORT);
+});
