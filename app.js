a035ce5fe60c6a87f4b27320d3b36e1a379b5a8a
Kaustuva Ghosh <ghosh.kaustuva@gmail.com>
2019-05-30 11:59:53 +0000

    first commit

--- app.js
+++ app.js
@@ -9,26 +9,29 @@ app.use(express.static(__dirname+"/public"));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 var usercount=0;
+var sent;
 app.use(function(req, res, next){
     // check if client sent cookie
       var cookie = req.cookies.usercount;
       if (cookie === undefined)
       {
         // no: set a new cookie
+        usercount++;
         res.cookie('usercount',usercount, { maxAge: 900000, httpOnly: true });
         console.log('cookie created successfully');
-        usercount++;
+        sent=usercount;
       } 
       else
       {
         // yes, cookie was already present 
         console.log('cookie exists', cookie);
+        sent=req.cookies.usercount;
       } 
     next();
 });
 
 app.get("/",function(req,res){
-    res.render("index",{usercount:usercount});
+    res.render("index",{usercount:sent});
 });
 
 
---
+++ readme.md
@@ -0,0 +1,3 @@
+This website uses cookies to keep a count of visitors.
+If a cookie is detected, then the visitor number is derived from the stored cookie
+if the cookie does not exist it will create it and increment the number.
